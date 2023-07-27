eb0ebc99c315d4e451881c130bd7ef1d1ceb38a9
Jacob Campos <3jacoblcampos@gmail.com>
2023-07-27 06:16:02 -0500

    kinda signupform

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import { useState } from 'react';
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
 
 function App() {
+  const [token, setToken] = useState(null);
 
   return (
     <>
-      <div>
-
-      </div>
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </>
   )
 }
